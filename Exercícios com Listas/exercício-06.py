2670461e6a683996d488b191d20282fd3b9e12a0
IanStoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-29 17:09:35 -0300

    Update exercício-06.py
    Código refatorado:
    A lista de notas agora está dentro do loop for.
    Remoção do método clear
    Simplificação do loop for

--- Exercícios com Listas/exercício-06.py
+++ Exercícios com Listas/exercício-06.py
@@ -5,25 +5,23 @@ imprima o número de alunos com média maior ou igual a 7.0.
 
 # Declarando variáveis e listas para armazenar valores
 alunos = 1
-listaNotas = []
 todasMedias = []
 medias7 = []
 
 # Estabelecendo limite de entradas
 while alunos <= 10:
     print(f"Aluno {alunos}")
+    notas = []
     for i in range(4):
-        i += 1
         # Coletando as 4 notas de cada aluno e classificando a média
-        listaNotas.append(float(input(f"Insira sua {i}ª nota: ")))
-        if len(listaNotas) == 4:
-            media = sum(listaNotas) / 4
-            todasMedias.append(media)
-            if media >= 7:
-                medias7.append(media)
-            listaNotas.clear()
+        nota = float(input(f"Insira sua {i+1}ª nota: "))
+        notas.append(nota)
+    media = sum(notas) / len(notas)
+    todasMedias.append(media)
+    if media >= 7:
+        medias7.append(media)
     alunos += 1
-alunos7 = len(medias7)
 
 # Mostrando resultados
+alunos7 = len(medias7)
 print(f"O número de alunos com média maior ou igual a 7 é de: {alunos7}")
