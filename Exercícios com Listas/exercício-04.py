8066b6a6fc1a4e6b8797b819f10cd4057729b2fa
IanStoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-29 17:02:40 -0300

    Update exercício-04.py
    Código refatorado:
    Criada lista de vogais ao invés de aninhar ifs para evitar repetição

--- Exercícios com Listas/exercício-04.py
+++ Exercícios com Listas/exercício-04.py
@@ -5,17 +5,13 @@ Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes 
 # Declarando uma lista de letras
 listaLetras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 
-# Comparando se a letra é vogal e removendo da lista
-if ("a") in listaLetras:
-    listaLetras.remove("a")
-if ("e") in listaLetras:
-    listaLetras.remove("e")
-if ("i") in listaLetras:
-    listaLetras.remove("i")
-if ("o") in listaLetras:
-    listaLetras.remove("o")
-if ("u") in listaLetras:
-    listaLetras.remove("u")
+# Criando uma lista de vogais
+vogais = ["a", "e", "i", "o", "u"]
+
+# Removendo as vogais da lista de letras
+for vogal in vogais:
+    if vogal in listaLetras:
+        listaLetras.remove(vogal)
 
 # Mostrando resultados
 print(f"Foram lidas {len(listaLetras)} consoantes: {listaLetras}")
