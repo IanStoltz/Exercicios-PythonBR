a76dcf5e5bd71ea812d9300640747235b8549c77
IanStoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-29 17:30:53 -0300

    Update exercício-14.py
    Código refatorado:
    Corrigido erro de sintaxe nas perguntas
    Utilização do método lower para converter os inputs

--- Exercícios com Listas/exercício-14.py
+++ Exercícios com Listas/exercício-14.py
@@ -6,33 +6,33 @@ Se a pessoa responder positivamente a 2 questões ela deve ser classificada como
 Caso contrário, ele será classificado como "Inocente".
 """
 
-# Criando lista para armazenar as respostas
+# Criando uma lista para armazenar as respostas
 respostas = []
 
-# Iniciando questionamentos e armazenando resposta na lista
+# Iniciando o interrogatório e armazenando as respostas na lista
 print("Interrogatório. Digite S para sim e N para não")
 p1 = input("Telefonou para a vítima? ")
-if p1 == "S" or p1 == "s":
+if p1.lower() == "s":
     respostas.append(p1)
 p2 = input("Esteve no local do crime? ")
-if p2 == "S" or p2 == "s":
+if p2.lower() == "s":
     respostas.append(p2)
 p3 = input("Mora perto da vítima? ")
-if p3 == "S" or p3 == "s":
+if p3.lower() == "s":
     respostas.append(p3)
-p4 = input("Mora perto da vítima? ")
-if p4 == "S" or p4 == "s":
+p4 = input("Devia para a vítima? ")
+if p4.lower() == "s":
     respostas.append(p4)
-p5 = input("Mora perto da vítima? ")
-if p5 == "S" or p5 == "s":
+p5 = input("Já trabalhou com a vítima? ")
+if p5.lower() == "s":
     respostas.append(p5)
 
-# Comparando número de respostas, classificando e mostrando o resultado
+# Comparando o número de respostas, classificando e mostrando o resultado
 if len(respostas) == 5:
     print("Assassino")
-elif len(respostas) <= 4 and len(respostas) >= 3:
+elif 3 <= len(respostas) <= 4:
     print("Cúmplice")
-elif len(respostas) < 3 and len(respostas) > 1:
+elif 1 < len(respostas) < 3:
     print("Suspeito")
 else:
     print("Inocente")
