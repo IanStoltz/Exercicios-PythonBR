72d0370cd2dfe0c70a9a692af0e55938efd93bef
IanStoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-29 17:17:25 -0300

    Update exercício-09.py
    Código refatorado:
    Remoção do incremento à variável i, por ser desnecessário.
    Variáveis renomeadas.

--- Exercícios com Listas/exercício-09.py
+++ Exercícios com Listas/exercício-09.py
@@ -2,19 +2,18 @@
 Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do vetor.
 """
 # Importando numpy
-import numpy
+import numpy as np
 
-# Criando lista para armazenar os valores
+# Criando uma lista para armazenar os valores
 nums = []
 
 # Coletando os valores e armazenando na lista
 for i in range(10):
     nums.append(int(input("Insira um número inteiro: ")))
-    i += 1
 
-# Calculando quadrado usando numpy e realizando a soma dos valores
-quadrado = numpy.square(nums)
-soma = sum(quadrado)
+# Calculando o quadrado usando numpy e realizando a soma dos valores
+quadrados = np.square(nums)
+soma = np.sum(quadrados)
 
 # Mostrando os resultados
 print(f"Os números inseridos foram: {nums}")
