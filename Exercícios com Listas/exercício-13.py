8f20764dbbad62cda03f837e226bd454fbc5cf76
IanStoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-29 17:27:05 -0300

    Update exercício-13.py
    Código refatorado:
    Iterando diretamente em meses, ao invés de usar o contador n
    Uso da função zip para iterar meses e temperaturas

--- Exercícios com Listas/exercício-13.py
+++ Exercícios com Listas/exercício-13.py
@@ -21,22 +21,17 @@ meses = [
     "Dezembro",
 ]
 media = 0
-n = 0
-acimaMedia = {}
 
 # Coletando e armazenando as temperaturas
-for i in range(len(meses)):
-    temperatura.append(float(input(f"Insira a temperatura de {meses[n]}: ")))
-    media += temperatura[i]
-    n += 1
+for mes in meses:
+    temperatura.append(float(input(f"Insira a temperatura de {mes}: ")))
+    media += temperatura[-1]
 
 # Calculando a média das temperaturas
 media = media / len(meses)
 
 # Comparando temperaturas acima da média
-for i in range(len(meses)):
-    if temperatura[i] > media:
-        acimaMedia.update({meses[i]: temperatura[i]})
+acimaMedia = {mes: temp for mes, temp in zip(meses, temperatura) if temp > media}
 
 # Mostrando os resultados
 print(f"Média das temperaturas anuais: {media:.2f}")
