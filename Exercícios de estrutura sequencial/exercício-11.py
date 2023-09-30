f9ebba9bea71783b5cc8c492d7ef0809e84400c9
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 13:49:41 -0300

    Update exercício-11.py
    Atualização dos comentários

--- Exercícios de estrutura sequencial/exercício-11.py
+++ Exercícios de estrutura sequencial/exercício-11.py
@@ -4,17 +4,17 @@ O produto do dobro do primeiro com metade do segundo .
 A soma do triplo do primeiro com o terceiro.
 O terceiro elevado ao cubo.
 '''
-# Coletando os valores dos números
+# Coleta os valores dos números
 inteiro = int(input("Insira um número inteiro: "))
 inteiro1 = int(input("Insira mais um número inteiro: "))
 real = float(input("Insira um número real: "))
 
-# Calculando produto, soma e exponenciação
+# Calcula o produto, a soma e a exponenciação
 produto = (2 * inteiro) + (inteiro1 / 2)
 soma = (3 * inteiro) + real
 exponenciacao = (real**3)
 
-# Mostrando os resultados
+# Mostra os resultados
 print(f'O produto do dobro do primeiro com metade do segundo é: {produto}')
 print(f'A soma do triplo do primeiro com o terceiro é: {soma}')
 print(f'O resultado do terceiro elevado ao cubo é: {exponenciacao}')
