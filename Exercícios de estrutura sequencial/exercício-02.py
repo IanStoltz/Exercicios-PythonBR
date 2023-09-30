a89b9e5440a8140e251703d001d9c5d21d74bb4e
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 09:51:24 -0300

    Add files via upload

---
+++ Exercícios de estrutura sequencial/exercício-02.py
@@ -0,0 +1,7 @@
+''' Faça um Programa que peça um número e então mostre a mensagem o número informado foi [número]. '''
+
+# Coletando número
+num = input("Insira um número qualquer: ")
+
+# Mostrando resultado
+print (f'O número informado foi [{num}]')
