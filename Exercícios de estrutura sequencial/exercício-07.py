cb9fc3e8379db515970f7981736b2f2155ad1ae1
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 13:48:26 -0300

    Update exercício-07.py
    Atualização dos comentários

--- Exercícios de estrutura sequencial/exercício-07.py
+++ Exercícios de estrutura sequencial/exercício-07.py
@@ -1,11 +1,11 @@
 '''Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.'''
 
-# Coletando valor do lado do quadrado
+# Coleta o valor do lado do quadrado
 lado = float(input("Insira o tamanho do lado do quadrado: "))
 
-# Calculando a área e o dobro
+# Calcula a área e o dobro
 area = lado ** 2
 dobro = area * 2
 
-# Mostrando o resultado
+# Mostra o resultado
 print (f'O dobro do valor da área é de: {dobro}')
