cb1bc0e6615a37d3032503194b191ca2075c3ddc
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 17:07:59 -0300

    Update exercício-07.py
    Comentários atualizados

--- Exercícios com Listas/exercício-07.py
+++ Exercícios com Listas/exercício-07.py
@@ -2,21 +2,21 @@
 Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
 """
 
-# Importando numpy para utilizar o método numpy.prod(lista)
+# Importa numpy para utilizar o método numpy.prod(lista)
 import numpy
 
-# Criando lista para armazenar os números
+# Cria lista para armazenar os números
 nums = []
 
-# Coletando os números
+# Coleta os números
 for i in range(5):
     nums.append(int(input("Insira um número inteiro: ")))
     i += 1
 
-# Calculando a soma e a multiplicação
+# Calcula a soma e a multiplicação
 soma = sum(nums)
 multiplica = numpy.prod(nums)
 
-# Mostrando os resultados
+# Mostra os resultados
 print(f"Os números inseridos foram: {nums}")
 print(f"A soma dos números é: {soma}, a multiplicação é: {multiplica}")
