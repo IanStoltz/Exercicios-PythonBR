6878cb86b853439d8079884f6356111621b83d3f
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 13:53:37 -0300

    Update exercício-18.py
    Atualização dos comentários

--- Exercícios de estrutura sequencial/exercício-18.py
+++ Exercícios de estrutura sequencial/exercício-18.py
@@ -3,12 +3,12 @@ Faça um programa que peça o tamanho de um arquivo para download (em MB) e a ve
 Calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 '''
 
-# Coletando valores de tamanho do arquivo e velocidade da conexão
+# Coleta os valores de tamanho do arquivo e velocidade da conexão
 tamanho = float(input("Insira, em MB, o tamanho do arquivo que será baixado: "))
 velocidade = float(input("Insira a velocidade da sua conexão em Mbps: "))
 
-# Calculando tempo necessário
+# Calcula o tempo necessário
 tempo = (tamanho / velocidade) / 60
 
-# Mostrando o resultado
+# Mostra o resultado
 print(f"O tempo estimado de download de {tamanho} MB, a {velocidade} Mbps, é de {tempo} minutos")
