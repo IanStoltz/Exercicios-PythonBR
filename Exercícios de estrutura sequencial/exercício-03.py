3c96aa99d90131949bea076b63e029db3c4292e3
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 09:51:45 -0300

    Add files via upload

---
+++ Exercícios de estrutura sequencial/exercício-03.py
@@ -0,0 +1,8 @@
+'''Faça um Programa que peça dois números e imprima a soma.'''
+
+# Coletando números
+num = float(input("Insira um número qualquer: "))
+num1 = float(input("Insira outro número qualquer: "))
+
+# Mostrando resultado
+print (f'A soma dos dois número inseridos é: {(num + num1)}')
