529954c54b2f0c9764cac99d274a48c636612efe
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 09:53:10 -0300

    Add files via upload

---
+++ Exercícios de estrutura sequencial/exercício-05.py
@@ -0,0 +1,10 @@
+'''Faça um Programa que converta metros para centímetros.'''
+
+# Coletando valor em metros
+metros = float(input("Insira seu valor em metros: "))
+
+# Realizando a conversão
+conversao = metros * 100
+
+# Mostrando resultado
+print ("Seu valor convertido para centímetros é de: ", conversao)
