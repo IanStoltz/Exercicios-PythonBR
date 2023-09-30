f62620f3e4f689c1f2a6f8fbd3da65bb55333038
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 09:52:46 -0300

    Add files via upload

---
+++ Exercícios de estrutura sequencial/exercício-04.py
@@ -0,0 +1,13 @@
+'''Faça um Programa que peça as 4 notas bimestrais e mostre a média.'''
+
+# Coletando as notas
+nota1 = float(input("Insira sua primeira média: "))
+nota2 = float(input("Insira sua segunda média: "))
+nota3 = float(input("Insira sua terceira média: "))
+nota4 = float(input("Insira sua última média: "))
+
+# Calculando a média
+mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4
+
+# Mostrando o resultado
+print("Sua média final é de: ", mediaFinal)
