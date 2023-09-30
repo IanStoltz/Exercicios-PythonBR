a67a990621202d276391c6efe690fb25ecddfbb3
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 17:24:05 -0300

    Update exercício-24.py
    Comentários atualizados

--- Exercícios com Listas/exercício-24.py
+++ Exercícios com Listas/exercício-24.py
@@ -4,17 +4,17 @@ Depois, mostre quantas vezes cada valor foi conseguido.
 Dica: use um vetor de contadores(1-6) e uma função para gerar numeros aleatórios, simulando os lançamentos dos dados.
 '''
 
-# Importando random para gerar números aleatórios
+# Importa random para gerar números aleatórios
 import random
 
-# Gerando lista de números
+# Geran lista de números
 numeros = [0] * 6
 
-# Lançando 100 vezes e adicionando nas listas
+# Lança o dado 100 vezes e armazena o valor nas listas
 for _ in range(1, 100):
     n = random.randint(1, 6)
     numeros[n - 1] += 1
 
-# Mostrando resultados
+# Mostra os resultados
 for i, n in enumerate(numeros):
     print(f'O número {i+1} foi lançado {n} vezes.')
