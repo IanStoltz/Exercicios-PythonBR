49274524d8a797e85ee3e5210bfd1f3a34a9f4d0
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 13:49:04 -0300

    Update exercício-09.py
    Atualização dos comentários

--- Exercícios de estrutura sequencial/exercício-09.py
+++ Exercícios de estrutura sequencial/exercício-09.py
@@ -1,10 +1,10 @@
 '''Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.'''
 
-# Coletando valor em Fahrenheit
+# Coleta o valor em Fahrenheit
 fahrenheit = float(input("Insira a temperatura em F°: "))
 
-# Convertendo valor para Celsius
+# Converte o valor para Celsius
 conversao = 5 * ((fahrenheit - 32)/9)
 
-# Mostrando o resultado
+# Mostra o resultado
 print (f'A temperatura de {fahrenheit}°F convertida para Celsius é: {conversao}°C')
