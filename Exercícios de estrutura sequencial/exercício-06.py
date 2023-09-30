36cb9bd0f1c70b2723af3816670ea17f66f86a2c
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 13:48:08 -0300

    Update exercício-06.py
    Atualização dos comentários

--- Exercícios de estrutura sequencial/exercício-06.py
+++ Exercícios de estrutura sequencial/exercício-06.py
@@ -1,10 +1,10 @@
 '''Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.'''
 
-# Coletando valor do raio
+# Coleta o valor do raio
 raio = float(input("Insira o valor do raio do círculo: "))
 
-# Calculando área
+# Calcula a área
 area = 3.1415 * (raio**2)
 
-# Mostrando resultado
+# Mostra o resultado
 print (f'O valor da área do círculo é de: {area:.2f}')
