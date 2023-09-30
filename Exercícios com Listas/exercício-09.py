9cc22fc7f544d6745eb508b88c135dcf852e53aa
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 17:08:47 -0300

    Update exercício-09.py
    Comentários atualizados

--- Exercícios com Listas/exercício-09.py
+++ Exercícios com Listas/exercício-09.py
@@ -1,20 +1,20 @@
 """
 Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do vetor.
 """
-# Importando numpy
+# Importa numpy
 import numpy as np
 
-# Criando uma lista para armazenar os valores
+# Cria uma lista para armazenar os valores
 nums = []
 
-# Coletando os valores e armazenando na lista
+# Coleta os valores e armazena na lista
 for i in range(10):
     nums.append(int(input("Insira um número inteiro: ")))
 
-# Calculando o quadrado usando numpy e realizando a soma dos valores
+# Calcula o quadrado usando numpy e realiza a soma dos valores
 quadrados = np.square(nums)
 soma = np.sum(quadrados)
 
-# Mostrando os resultados
+# Mostra os resultados
 print(f"Os números inseridos foram: {nums}")
 print(f"A soma dos quadrados dos números é: {soma}")
