dab62582b72dddb2f90525b852c89709dfd11809
Ian Rodrigo Stoltz <116393728+IanStoltz@users.noreply.github.com>
2023-09-30 13:51:39 -0300

    Update exercício-16.py
    Atualização dos comentários

--- Exercícios de estrutura sequencial/exercício-16.py
+++ Exercícios de estrutura sequencial/exercício-16.py
@@ -3,15 +3,15 @@ Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em
 Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, 
 que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
 '''
-# Importando math para arredondar caso necessário
+# Importa math para arredondar caso necessário
 import math
 
-# Coletando valor da área
+# Coleta o valor da área
 area = int(input("Insira a área a ser pintada, em metros quadrados : "))
 
-# Calculando número de latas necessárias e valor
+# Calcula o número de latas necessárias e o valor
 latas = area / 54
 valor = 80 * latas
 
-# Imprimindo o resultado
+# Mostra o resultado
 print (f'Para a área de {area} metros quadrados serão necessárias {math.ceil(latas)} lata(s) de 18 litros. Saindo por R$ {valor:.2f}')
